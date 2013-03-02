95767d6e553574d7bcca5612376915b593fe8894
guitarman <janototh@gmail.com>
2013-03-02 17:22:04 +0100

    added gem ancestry, has_ancestry, validates, attr_accessible to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ActiveRecord::Base
+  attr_accessible :name, :parent_id
+
+  validates :name, :presence => true
+
+  has_ancestry
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130302134725) do
+ActiveRecord::Schema.define(:version => 20130302142437) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "ancestry"
   end
+
+  add_index "categories", ["ancestry"], :name => "index_categories_on_ancestry"
 
 end
