52157582d4251804e0a9568b2b0c4d1dc2a9219b
guitarman <janototh@gmail.com>
2013-03-02 14:41:27 +0100

    first init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreeFilter::Application.config.secret_token = 'f29c5024038c4345453d0077770057c419e89dae13c96d6aea49b2005c0cd2f7df2b2688ec66b5860bcdadbeccbd4f32d0aebb30d337ca41b531d8b04845c788'
