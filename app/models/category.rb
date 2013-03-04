a7a1fa8db835c50e6c383c3a22add70a2a43ac6c
guitarman <janototh@gmail.com>
2013-03-04 23:12:02 +0100

    new scope :with_ids returns categories with given ids, search methods will now also find all uniqe missing parents #TODO join @categories and @parents

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,13 +7,37 @@ class Category < ActiveRecord::Base
   has_ancestry :orphan_strategy => :destroy
 
   scope :with_name, lambda { |name| {:conditions => ['name LIKE ?', "%#{name}%"]} }
+  scope :with_ids, lambda { |ids| {:conditions => {:id => ids } } }
 
   def self.search(search)
     if search.blank?
       scoped
     else
-      #TODO find path to root elements
+      #TODO join @categories and @parents
       @categories = Category.with_name(search)
+
+      parents_ids = []
+      children_ids = []
+
+      #find all parents ids and children ids
+      @categories.each do |category|
+        ancestry = category.ancestry
+        parents_ids += ancestry.split("/") unless ancestry.nil?
+        children_ids << category.id.to_s
+      end
+
+      if parents_ids.any?
+        #make parent_ids unique
+        parents_ids.uniq!
+        #delete parents, which were already found via search parameter
+        parents_ids = parents_ids - children_ids
+        #select all unique missing parents
+        @parents = Category.with_ids(parents_ids)
+
+        #join @parents and @categories
+      end
+
+      @categories
     end
   end
 end
